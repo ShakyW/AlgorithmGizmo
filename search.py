86794094d533218b0b4857912e3c80413b216cfb
James Greene <james.joshua.greene@gmail.com>
2024-04-10 11:34:28 -0400

    Track number of nodes searched

--- search.py
+++ search.py
@@ -13,20 +13,28 @@ def main():
     if (choiche.lower() == 'b'):
         fringe = [*getNeighbors(startPos, WIDTH, HEIGHT)]
         isDone = 0
+        total = 0
 
         while(not isDone): # Until we are finished loop through the BFS
             isDone = bfsStep(board, fringe, endPos, WIDTH, HEIGHT)
+            total += not isDone
             print_graph_graph_supplied(board)
             input()
 
+        print('Total nodes searched: ', total)
+
     if (choiche.lower() == 'd'):
         stack = [('E', *idxToRowCol(startPos, WIDTH))]
+        total = 0
         while stack:
             board, stack, flag = dfs_step(board, stack)
             print_graph_graph_supplied(board)
             input()
+            total += not flag
             if flag:
-                return
+                break
+
+        print('Total nodes searched: ', total)
             
     if (choiche.lower() == 'm'):
         total = 0
